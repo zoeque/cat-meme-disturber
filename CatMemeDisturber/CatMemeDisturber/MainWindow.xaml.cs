57bb87c56b50371551141627e158bcb5a2ea8d72
zoeque <121230485+zoeque@users.noreply.github.com>
2024-03-15 21:49:56 +0900

    update:show cat when the window is closed

--- CatMemeDisturber/CatMemeDisturber/MainWindow.xaml.cs
+++ CatMemeDisturber/CatMemeDisturber/MainWindow.xaml.cs
@@ -21,16 +21,37 @@ namespace CatMemeDisturber
             InitializeComponent();
         }
 
+        /// <summary>
+        /// The button activity when the happy cat button is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void HappyCat_Button_Click(object sender, RoutedEventArgs e)
         {
             HappyCat cat = new HappyCat();
             cat.Show();
         }
 
+        /// <summary>
+        /// The button activity when the goat button is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Goat_Button_Click(object sender, RoutedEventArgs e)
         {
             Goat goat = new Goat();
             goat.Show();
+        }
+
+        /// <summary>
+        /// Show the happy cat when the button menu is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            HappyCat cat = new HappyCat();
+            cat.Show();
         }
     }
 }
